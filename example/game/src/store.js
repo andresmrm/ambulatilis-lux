1c5092983c7cda709abb7729a4262f9342782bc8
Andres MRM <andres@inventati.org>
2016-01-19 14:31:44 -0200

    show that z works

--- example/game/src/store.js
+++ example/game/src/store.js
@@ -1,6 +1,6 @@
 export var state = {
-    position: [1, 2, 0],
-    direction: 'w',
+    position: [0, 0, 0],
+    direction: 'e',
     // if is waiting the load of an image for the vision
     loadingVision: false,
 }
